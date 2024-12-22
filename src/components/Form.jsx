83798b9111326fad708c126ad1471ecb8d02bd56
Annie Collipp <anniecollipp@gmail.com>
2024-12-22 14:24:53 -0500

    Updates

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,20 +5,36 @@ function Form() {
 
     const sendEmail = (e) => {
         e.preventDefault()
-        emailjs.sendForm(EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, e.target, EMAILJS_PUBLIC_KEY)
-      }
+        console.log(EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, e.target, EMAILJS_PUBLIC_KEY)
+        // emailjs.sendForm(EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, e.target, EMAILJS_PUBLIC_KEY)
+    }
 
     return (
-        <div >
-          <form onSubmit={sendEmail}>
-            <label htmlFor="emailFrom">From: </label>
-            <input name="email_from" id="emailFrom"></input>
-            <label htmlFor="message">Message: </label>
-            <input name="message" id="message"></input>
-            <button type="submit">Send email</button>
-          </form>
-        </div>
-      );
+
+        <section className="rounded-md shadow-md">
+            <h2 className="text-lg font-semibold text-grey-90 capitalize">Account settings</h2>
+
+            <form onSubmit={sendEmail}>
+                <div className="gap-6 mt-4 ">
+                    <div>
+                        <label className="text-grey-90" htmlFor="emailAddress">Email Address</label>
+                        <input id="emailAddress" name="email_from" type="email" className="block w-full px-4 py-2 mt-2 text-grey-90 bg-white border border-gray-200 rounded-md dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                    </div>
+
+                    <div>
+                        <label className="text-grey-90" htmlFor="message">Message</label>
+                        <textarea id="message" name="message" className="block w-full px-4 py-2 mt-2 text-grey-90 bg-white border border-gray-200 rounded-md dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                    </div>
+
+                </div>
+
+                <div className="flex justify-end mt-6">
+                    <button type="submit" className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 
+                    transform bg-blue-50 rounded-md hover:bg-blue-70 focus:outline-none focus:bg-blue-70">Save</button>
+                </div>
+            </form>
+        </section>
+    );
 }
 
 export default Form;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,26 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      grey: {
+        90: "#1D1D1D",
+        50: "#737373"
+      },
+      blue: {
+        50: "#477AE0",
+        70: "#355BA7"
+      },
+      orange: {
+        20: "#E0583F",
+        80: "#6C2A1E"
+      },
+      green: {
+        40: "#88A155",
+        80: "#566636"
+      },
+      navy: "#55678B",
+      white: "#FFFFFF"
+    },
     extend: {},
   },
   plugins: [],
